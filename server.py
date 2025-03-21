07a72302c23c70e65a16250b41e933f0f589c735
ElMedoedo <ZebraSkill@yandex.ru>
2025-03-21 19:45:27 +0300

    Логика отображения переехала в шаблон. Т.е. все что связано с отображением - в шаблоне. А в index - то, где функция занимается подготовкой для отображания и передачи их в шаблон. Внутри шаблона мы говорим- как отображать данные

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
 from weather import weather_by_city
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    title = "Прогноз погоды" # Создание переменной для шаблона
-    weather=weather_by_city("Moscow,Russia")
-    if weather:
-        weather_text = f"Погода: {weather["temp_C"]}, ощущается как: {weather["FeelsLikeC"]}"  # внутри f строки - Показывание только нескольких ключей из словаря
-    else:
-        weather_text = "Сервис погоды временно недоступен"
-    return render_template("index.html", page_title=title, weather=weather_text) # Передаем в шаблон то, что мы присваеваем тут, а именно title и weather_text
+    title = "Прогноз погоды"  # Создание переменной для шаблона
+    weather = weather_by_city("Moscow,Russia")
+    return render_template(
+        "index.html", page_title=title, weather=weather
+    )  # Передаем в шаблон то, что мы присваеваем тут, а именно title и weather_text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
